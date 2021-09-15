7c7a20cebb3901144de25c9f4894429226d958a3
Marco Melnykovych <g.marex@gmail.com>
2021-09-15 15:52:15 +0200

    complete reliability filtering

--- db_files/queries/city_population.sql
+++ db_files/queries/city_population.sql
@@ -10,10 +10,10 @@ FROM
 	WHERE
 		LOWER(city_name) = '{0}'
 		AND year <= {1}
-		AND city_type = 'City proper'
+		{2}AND city_type = 'City proper'
 	) my
 		ON my.year = p.year
 WHERE
 	LOWER(p.city_name) = '{0}'
 	AND p.year <= {1}
-	AND p.city_type = 'City proper';
+	{3}AND p.city_type = 'City proper';
--- utilities/api_tools.py
+++ utilities/api_tools.py
@@ -16,18 +16,26 @@ class CitiesAPI(Resource):
             city_name = params['CityName'].lower()
         except KeyError:
             return 'Bad request', 400
+        prov_filt_nest = ''
+        prov_filt = ''
+        year_filt = 9999
         if 'year' in params.keys():
             try:
                 year_filt = int(params["year"])
             except ValueError:
                 return 'Bad request', 400
-        else:
-            year_filt = 9999
+        if 'includeProvisional' in params.keys():
+            prov_val = params['includeProvisional']
+            if prov_val not in ('true', 'false'):
+                return 'Bad request', 400
+            elif prov_val == 'false':
+                prov_filt_nest = 'AND reliability = "Final figure, complete" '
+                prov_filt = 'AND p.reliability = "Final figure, complete" '
         query_path = '../db_files/queries/city_population.sql'
         with open(query_path, 'r') as query_file:
             query_base = query_file.read()
         with DBMaster('cities_api_db') as dbm:
-            sql_res = dbm.fetch_data(query=query_base.format(city_name, year_filt))
+            sql_res = dbm.fetch_data(query=query_base.format(city_name, year_filt, prov_filt_nest, prov_filt))
         return {'population': sql_res[0][0]}
 
     def post(self):
